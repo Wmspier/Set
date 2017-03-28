11d93d5b3006c7cc017e2a0a5f196f927757f95c
William <william.spier@mymail.champlain.edu>
2017-03-27 23:58:11 -0400

    Implemented a Board Controller that initializes the cards with a margin. It took way too long to scale those RectTransforms for some reason...¯\_(ツ)_/¯

---
+++ Set/Assets/Scripts/BoardController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardController : MonoBehaviour {
+
+    private GameObject[][] m_grid;
+
+    public int DefaultWidth;
+    public int DefaultHeight;
+    public float CardMargin;
+
+    public GameObject CardPrefab;
+
+
+	void Start () 
+    {
+        GenerateGrid();
+    
+    }
+	void Update () { }
+
+
+    void GenerateGrid()
+    {
+        RectTransform thisTransform = gameObject.GetComponent<RectTransform>();
+
+        for (int i = 0; i < DefaultWidth; i++)
+        {
+            for (int j = 0; j < DefaultHeight; j++)
+            {
+                GameObject newCard = Instantiate(CardPrefab);
+                RectTransform newCardTransform = newCard.GetComponent<RectTransform>();
+                newCardTransform.SetParent(thisTransform, false);
+                //Insert card rect into parent rect           //Start from left anchor  //Add a margin // Set the width of the card 
+                newCardTransform.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, CardMargin/2, thisTransform.rect.width / DefaultWidth - CardMargin);
+                newCardTransform.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Bottom, CardMargin/2, thisTransform.rect.height / DefaultHeight - CardMargin);
+                //Set the position of the card rect relative to this rect
+                newCardTransform.position = new Vector2(newCardTransform.position.x + (thisTransform.rect.width / DefaultWidth * i * newCardTransform.lossyScale.x),
+                                                        newCardTransform.position.y + (thisTransform.rect.height / DefaultHeight * j * newCardTransform.lossyScale.y));
+                newCard.name = "Card_" + (i*DefaultHeight + j);
+            }
+        }
+    }
+}
